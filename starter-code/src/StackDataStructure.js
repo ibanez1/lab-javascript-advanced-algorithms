823fff1831b5d2af1f8e1d44c1dc6c831fb7bcd8
Alejandro <alexibape@hotmail.com>
2018-03-28 22:30:51 +0200

    Hasta Jasmine Stack properties. Me ha costado mucho concentrarme

--- starter-code/src/StackDataStructure.js
+++ starter-code/src/StackDataStructure.js
@@ -1,2 +1,47 @@
 function StackDataStructure () {
+  this.stackControl = [];
+  this.MAX_SIZE = 0
+};
+
+StackDataStructure.prototype.isEmpty  = function (){
+   if (this.stackControl.length == 0) {
+     return true;
+   }
+   else {
+     return false
+   }
+
+};
+
+StackDataStructure.prototype.canPush = function(){
+  if (this.MAX_SIZE < 1) {
+    return true
+  }
+  else{
+    return false
+  }
 }
+
+
+StackDataStructure.prototype.push = function (element) {
+  
+    if (this.canPush()) {
+      this.stackControl.push(element);
+      return this.stackControl;
+    } else {
+      return "Stack Overflow";
+    }
+  };
+
+
+StackDataStructure.prototype.pop = function (){
+
+      if (this.stackControl.length !== 0){   
+           return this.stackControl.pop()
+          }
+      else {
+        return "Stack Underflow"
+      }    
+}
+
+
