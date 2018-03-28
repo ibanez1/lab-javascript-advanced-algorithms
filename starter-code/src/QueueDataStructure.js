42c144592893b6912dffaef76e2ffc9b633e98e2
Alejandro <alexibape@hotmail.com>
2018-03-28 23:01:36 +0200

    Jasmine Terminado

--- starter-code/src/QueueDataStructure.js
+++ starter-code/src/QueueDataStructure.js
@@ -1,2 +1,49 @@
 function QueueDataStructure () {
+
+  this.queueControl = [];
+  this.MAX_SIZE = 0
 }
+
+QueueDataStructure.prototype.isEmpty  = function (){
+  if (this.queueControl.length == 0) {
+    return true;
+  }
+  else {
+    return false
+  }
+
+};
+
+QueueDataStructure.prototype.canEnqueue = function(){
+
+  if (this.MAX_SIZE < 1) {
+    return true
+  }
+  else{
+    return false
+  }
+}
+
+
+
+QueueDataStructure.prototype.enqueue = function (element) {
+ 
+   if (this.canEnqueue()) {
+     this.queueControl.push(element);
+     return this.queueControl.reverse();
+   } else {
+     return "Queue Overflow";
+   }
+ };
+
+
+ QueueDataStructure.prototype.dequeue = function (){
+
+     if (this.queueControl.length !== 0){   
+          return this.queueControl.pop()
+         }
+     else {
+       return "Queue Underflow"
+     }    
+}
+
